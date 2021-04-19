70a47b85c026dd3c9ba235488f183c8f8e3ca7f9
LaviBenshimol <57056914+LaviBenshimol@users.noreply.github.com>
2021-04-19 17:38:02 +0300

    loading API-Gatway log entry

--- jsonLogParser.py
+++ jsonLogParser.py
@@ -13,6 +13,27 @@ with open("data_file.json", "w") as write_file:
 with open("data_file.json", "r") as read_file:
     data = json.load(read_file)
 
+
+# ##################################################
+#
+#     Taxonomy layer:
+#
+#         "evenName": "Invoke"
+# ##################################################
+
+def TaxonomyLayer(data):
+    #Entity keys:
+    data_entries = data.keys()
+    identity_entity = data.items()
+    for i in data.get(userIdentity)
+        #if its AWSService
+        #type -> type
+        #invokedBy -> name
+        entity.append = data.get("userIdentity")
+
+    return ans
+
+
 # ##################################################
 #
 #     Decoding logs:
@@ -24,3 +45,5 @@ with open("logEntry.json", "r") as currentLog:
     data = json.load(currentLog)
 
 print(data)
+nodes, edges = TaxonomyLayer(data)
+#update DB (create/update new nodes/edges)

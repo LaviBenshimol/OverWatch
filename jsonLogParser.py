fbf8e8b9450a5f20ba6192f8a69a6e8f97f3110f
LaviBenshimol <57056914+LaviBenshimol@users.noreply.github.com>
2021-04-25 16:48:36 +0300

    building specific log entry. need to test function

--- jsonLogParser.py
+++ jsonLogParser.py
@@ -1,11 +1,14 @@
 import json
+
 from neo4j import __version__ as neo4j_version, GraphDatabase
 from pandas import DataFrame
+from py2neo import Graph, Node, Relationship, NodeMatcher
 
 print(neo4j_version)
 
+
 ###############################################################################################
-             # START of working in neo4j
+# START of working in neo4j
 ###############################################################################################
 class Neo4jConnection:
 
@@ -36,8 +39,9 @@ class Neo4jConnection:
             if session is not None:
                 session.close()
         return response
-def neo4j_add_new_log():
 
+
+def neo4j_add_new_log(conn):
     conn.query("CREATE OR REPLACE DATABASE coradb")
 
     query_string = '''
@@ -101,9 +105,10 @@ def neo4j_add_new_log():
     conn.close()
     return
 
+
 ###############################################################################################
-             # END of working in neo4j
-             # conn = Neo4jConnection(uri="bolt://localhost:7687", user="superman", pwd="pizza")
+# END of working in neo4j
+# conn = Neo4jConnection(uri="bolt://localhost:7687", user="superman", pwd="pizza")
 ###############################################################################################
 ###################################################
 #     Taxonomy layer:
@@ -123,54 +128,93 @@ def neo4j_add_new_log():
 #             userAgent:
 #             status: ? what info to inspect and record?
 ###################################################
-def userIdentity_Parser(data,dbconnection):
-    query_string = '''
-        CREATE (:Paper {id: line.paper_id, class: line.label})
-        '''
-    name = data['userIdentity']['invokedBy']
-    # MERGE(n {name: '3'})
-    dbconnection.query(query_string, db='logstesting')
-    query_string = '''
-        USING PERIODIC COMMIT 500
-        LOAD CSV WITH HEADERS FROM
-        'https://raw.githubusercontent.com/ngshya/datasets/master/cora/cora_cites.csv'
-        AS line FIELDTERMINATOR ','
-        MATCH (citing_paper:Paper {id: line.citing_paper_id}),(cited_paper:Paper {id: line.cited_paper_id})
-        CREATE (citing_paper)-[:CITES]->(cited_paper)
-        '''
-    dbconnection.query(query_string, db='logstesting')
-    return
-def eventSource_Parser(data,dbconnection):
+# def nodeExist(lbl, node):
+#     matcher = NodeMatcher(graph)
+#     m = matcher.match(lbl, screen_name == node.screen_name).first()
+#     if m is None:
+#        return False
+#     else:
+#        return True
+def user_identity_parser(data_base_connection, entity_name, entity_type, arn, region):
+    #
+    # name = data['userIdentity']['invokedBy']
+    # name = name.partition('.')[0]
+    # type = data['userIdentity']['type']
+    # a = Node(name, name=name, type=type) #py2Neo
+    query_enter_identity = f"""
+    MERGE({str(entity_name)}: Service{{name: {str(entity_name)}, type: {str(entity_type)}, arn: {str(arn)}, region: {str(region)} }})
+    ON CREATE
+    SET {str(entity_name)}.created = timestamp()
+    ON MATCH
+    SET
+    {str(entity_name)}.lastSeen = timestamp()
+    RETURN {str(entity_name)}.name, {str(entity_name)}.created, {str(entity_name)}.lastSeen
+    """
 
-    return
-def link_Parser(data,dbconnection):
-
+    ans = data_base_connection.query(query_enter_identity, db='loglake')
     return
 
-def TaxonomyLayer(data,dbconnection):
-    dbconnection.query("CREATE OR REPLACE DATABASE logstesting")
-    #Entity keys:
-    userIdentity_Parser(data, dbconnection)
-    eventSource_Parser(data, dbconnection)
-    link_Parser(data, dbconnection)
-    data_entries = data.keys()
-    identity_entity = data.items()
-    # for i in data.get(userIdentity)
-    #     #if its AWSService
-    #     #type -> type
-    #     #invokedBy -> name
-    #     entity.append = data.get("userIdentity")
 
+def event_source_parser(data_base_connection, entity_source_name, arn, region):
+    query_enter_identity = f"""
+       MERGE({str(entity_source_name)}: Service{{name: {str(entity_source_name)},, arn: {str(arn)}, region: {str(region)} }})
+       ON CREATE
+       SET {str(entity_source_name)}.created = timestamp()
+       ON MATCH
+       SET
+       {str(entity_source_name)}.lastSeen = timestamp()
+       RETURN {str(entity_source_name)}.name, {str(entity_source_name)}.created, {str(entity_source_name)}.lastSeen
+       """
+
+    ans = data_base_connection.query(query_enter_identity, db='loglake')
     return
+
+
+def link_parser(data_base_connection,entity_name,entity_source_name, event_type, timestamp, source_ip, user_agent):
+    query_enter_identity = f""""
+    (MATCH(Service:{str(entity_source_name)}) - 
+    [:{str(event_type)}{{timestamp:{str(timestamp)},source_ip:{str(source_ip)},user_agent:{str(user_agent)}}}] 
+    -> (MATCH(Service:{str(entity_name)}
+    """
+    ans = data_base_connection.query(query_enter_identity, db='loglake')
+    return
+
+
+def TaxonomyLayer(data, data_base_connection):
+    data_base_connection.query("CREATE OR REPLACE DATABASE logslake")
+    # Entity keys:
+    user_identity_parser(data_base_connection=data_base_connection,
+                         entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
+                         entity_type=data['userIdentity']['type'],
+                         arn=data['requestParameters']['sourceArn'],
+                         region=data['awsRegion'])
+    event_source_parser(dbconnection=data_base_connection,
+                        entity_source_name=data['eventSource'],
+                        arn=data['resources']['ARN'],
+                        region=data['awsRegion'])
+    link_parser(data_base_connection=data_base_connection,
+                entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
+                entity_source_name=data['eventSource'],
+                event_type=data['eventType'],
+                timestamp=data['eventTime'],
+                source_ip=data['sourceIPAddress'],
+                user_agent=data['userAgent'])
+    return
+
 
 # Defining main function
 def main():
-    print("hey there")
-    dbconnection = Neo4jConnection(uri="bolt://localhost:7687", user="superman", pwd="pizza")
+    print("Connecting to neo4j DB...")
+    data_base_connection = Neo4jConnection(uri="bolt://localhost:7687", user="superman", pwd="pizza")
+    print("Connected")
+    print("Taxonomy for 1 log entry")
+    graph = Graph("bolt://localhost:7687", auth=("superman", "pizza"))
+
     with open("logEntry.json", "r") as read_file:
         data = json.load(read_file)
-    TaxonomyLayer(data, dbconnection)
+    TaxonomyLayer(data, data_base_connection)
     return
+
 
 # Using the special variable
 # __name__
@@ -235,6 +279,7 @@ if __name__ == "__main__":
 # #             Priviledge learning ?
 # #             Counters?
 # #         Edges:
+
 # #             Type: Relation(Trusted/Owned) / Event(type).
 # #             Timestamp: convert time to specified format.
 # #             Source IP:

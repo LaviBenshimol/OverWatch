32242741f48217ceb75d8059e307701db0bea117
LaviBenshimol <57056914+LaviBenshimol@users.noreply.github.com>
2021-04-26 17:03:50 +0300

    finisihed resovlving 1 entry log. need to do more..

--- jsonLogParser.py
+++ jsonLogParser.py
@@ -91,8 +91,10 @@ def print_node(data_base_connection, name):
 def user_identity_parser(data_base_connection, entity_name, entity_type, arn, region):
     if not node_exist(data_base_connection, entity_name):
         return
+    # entity_type = "aws service"
+    # entity_node = Node(entity_type,name=entity_name,{arn=str(arn),region=str(region)})
     query_enter_identity = f"""
-    CREATE (n:{str(entity_name)} {{name: '{str(entity_name)}', arn: '{str(arn)}, region: {str(region)}'}})
+    CREATE (n:{str(entity_type)} {{name: '{str(entity_name)}', arn: '{str(arn)}, region: {str(region)}'}})
     """
     data_base_connection.query(query_enter_identity, db='overwatchDataBase')
     print_node(data_base_connection, entity_name)
@@ -111,30 +113,39 @@ def user_identity_parser(data_base_connection, entity_name, entity_type, arn, re
 def event_source_parser(data_base_connection, entity_source_name, arn, region):
     if not node_exist(data_base_connection, entity_source_name):
         return
+    entity_type = "AWSService"              #TODO: how to figure out its entity type ? what label to put? to update?
     query_enter_identity = f"""
-        CREATE (n:{str(entity_source_name)} {{name: '{str(entity_source_name)}', arn: '{str(arn)}, region: {str(region)}'}})
+        CREATE (n:{str(entity_type)} {{name: '{str(entity_source_name)}', arn: '{str(arn)}, region: {str(region)}'}})
         """
-    # query_enter_identity = f"""
-    #    MERGE({str(entity_source_name)}: Service{{name: {str(entity_source_name)},, arn: {str(arn)}, region: {str(region)} }})
-    #    ON CREATE
-    #    SET {str(entity_source_name)}.created = timestamp()
-    #    ON MATCH
-    #    SET
-    #    {str(entity_source_name)}.lastSeen = timestamp()
-    #    RETURN {str(entity_source_name)}.name, {str(entity_source_name)}.created, {str(entity_source_name)}.lastSeen
-    #    """
-
     data_base_connection.query(query_enter_identity, db='overwatchDataBase')
     print_node(data_base_connection, entity_source_name)
     return
 
 
 def link_parser(data_base_connection, entity_name, entity_source_name, event_type, timestamp, source_ip, user_agent):
-    query_enter_identity = f""""
-    (MATCH(Service:{str(entity_source_name)}) - 
-    [:{str(event_type)}{{timestamp:{str(timestamp)},source_ip:{str(source_ip.replace(".","-"))},user_agent:{str(user_agent)}}}] 
-    -> (MATCH(Service:{str(entity_name)}
+    #TODO:retrive type ?
+    entity_type = "AWSService"
+    time_stamp = timestamp
+    if event_type == "AwsApiCall":
+        reltype='AWS_API_CALL'
+    else:
+        reltype='MANAGEMENT'
+
+    query_enter_identity = f"""
+    MATCH (a:{entity_type}),(b:{entity_type})
+    WHERE a.name = '{entity_name}' AND b.name = '{entity_source_name}'
+    CREATE (a)-[relation:{reltype}]->(b)
+    SET relation = {{Attributes:['{str(source_ip)}','{str(user_agent)}','{str(time_stamp)}']}}
     """
+    # SET relation = {{properties:[{str(time_stamp)},{str(source_ip).replace(".","--")},{str(user_agent).replace(".","--")}]}}
+    #time: [{str(time_stamp)}],
+    #srcIP: [{str(source_ip).replace(":","--")}],
+    #user_agent:[{user_agent.replace(":","--")}]}}]->(b)
+    # RETURN type(r)
+
+    # (MATCH(Service:{str(entity_source_name)}) -
+    # [:{str(event_type)}{{timestamp:{str(timestamp)},source_ip:{str(source_ip.replace(".","-"))},user_agent:{str(user_agent)}}}]
+    # -> (MATCH(Service:{str(entity_name)}
     ans = data_base_connection.query(query_enter_identity, db='overwatchDataBase')
     return
 
@@ -150,13 +161,13 @@ def taxonomy_layer(data, data_base_connection):
                         entity_source_name=data['eventSource'].partition('.')[0],
                         arn=data['resources'][0]['ARN'].replace(":", "--"),
                         region=data['awsRegion'])
-    # link_parser(data_base_connection=data_base_connection,
-    #             entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
-    #             entity_source_name=data['eventSource'],
-    #             event_type=data['eventType'],
-    #             timestamp=data['eventTime'],
-    #             source_ip=data['sourceIPAddress'],
-    #             user_agent=data['userAgent'])
+    link_parser(data_base_connection=data_base_connection,
+                entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
+                entity_source_name=data['eventSource'].partition('.')[0],
+                event_type=data['eventType'],
+                timestamp=data['eventTime'],
+                source_ip=data['sourceIPAddress'],
+                user_agent=data['userAgent'])
     return
 
 
@@ -166,7 +177,7 @@ def print_nodes(data_base_connection):
     """
     nodes = data_base_connection.query(print_all_nodes_query, db='overwatchDataBase')
     print("nodes:\n")
-    print(nodes)
+    print(str(nodes).replace(", ","\n"))
     print("\n")
     return
 
@@ -184,7 +195,6 @@ def main():
     ans = data_base_connection.query(query=show_me_the_databases, db="neo4j")
     print("show database Before: CREATE OR REPLACE \n" + str(ans))
 
-
     data_base_connection.query("CREATE OR REPLACE DATABASE overwatchDataBase")
     ans = data_base_connection.query(query=show_me_the_databases, db="overwatchDataBase")
     print("show database After: CREATE OR REPLACE \n" + str(ans))
@@ -198,7 +208,10 @@ def main():
     test = f"""{test}"""
     test.replace(":", "--")
     # print(test)
+    print("Before taxonomy layer")
+    print_nodes(data_base_connection)
     taxonomy_layer(data, data_base_connection)
+    print("After taxonomy layer")
     print_nodes(data_base_connection)
     print("complete")
     return

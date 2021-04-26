55dd60a6d92edf76e4b4998587ed249606fee9a3
LaviBenshimol <57056914+LaviBenshimol@users.noreply.github.com>
2021-04-26 13:08:05 +0300

    functions added: print_node print_nodes node_exist user_identity_parser event_source_parser

--- jsonLogParser.py
+++ jsonLogParser.py
@@ -66,31 +66,36 @@ def nodeExist(lbl, node):
        """
 
 
-def user_identity_parser(data_base_connection, entity_name, entity_type, arn, region):
-    #
-    # name = data['userIdentity']['invokedBy']
-    # name = name.partition('.')[0]
-    # type = data['userIdentity']['type']
-    # a = Node(name, name=name, type=type) #py2Neo]
-    query_enter_identity = f"""
-    MATCH (j {{name: '{entity_name}'}})
-    RETURN j.name
-    """
-    res = data_base_connection.query(query=query_enter_identity, db='overwatchDataBase')
+def node_exist(data_base_connection, name):
+    check_if_node_exist_query = f"""
+        MATCH (j: {name} {{name: "{name}"}})
+        RETURN j.name
+        """
+    res = data_base_connection.query(query=check_if_node_exist_query, db='overwatchDataBase')
     if res is None:
+        return False
+    else:
+        return True
+
+
+def print_node(data_base_connection, name):
+    query_enter_identity = f"""
+        MATCH (j {{name: '{name}'}})
+        RETURN j.name
+        """
+    res = data_base_connection.query(query=query_enter_identity, db='overwatchDataBase')
+    print("print node: " + str(res))
+    return
+
+
+def user_identity_parser(data_base_connection, entity_name, entity_type, arn, region):
+    if not node_exist(data_base_connection, entity_name):
         return
     query_enter_identity = f"""
     CREATE (n:{str(entity_name)} {{name: '{str(entity_name)}', arn: '{str(arn)}, region: {str(region)}'}})
     """
-
-    ans = data_base_connection.query(query_enter_identity, db='overwatchDataBase')
-    query_enter_identity = f"""
-    MATCH (j {{name: '{entity_name}'}})
-    RETURN j.name
-    """
-    res = data_base_connection.query(query=query_enter_identity, db='overwatchDataBase')
-    if res is None:
-        return
+    data_base_connection.query(query_enter_identity, db='overwatchDataBase')
+    print_node(data_base_connection, entity_name)
     # query_enter_identity = f"""
     # MERGE({str(entity_name)}: Service{{name: {str(entity_name)}, type: {str(entity_type)}, arn: {str(arn)}, region: {str(region)} }})
     # ON CREATE
@@ -104,24 +109,30 @@ def user_identity_parser(data_base_connection, entity_name, entity_type, arn, re
 
 
 def event_source_parser(data_base_connection, entity_source_name, arn, region):
+    if not node_exist(data_base_connection, entity_source_name):
+        return
     query_enter_identity = f"""
-       MERGE({str(entity_source_name)}: Service{{name: {str(entity_source_name)},, arn: {str(arn)}, region: {str(region)} }})
-       ON CREATE
-       SET {str(entity_source_name)}.created = timestamp()
-       ON MATCH
-       SET
-       {str(entity_source_name)}.lastSeen = timestamp()
-       RETURN {str(entity_source_name)}.name, {str(entity_source_name)}.created, {str(entity_source_name)}.lastSeen
-       """
+        CREATE (n:{str(entity_source_name)} {{name: '{str(entity_source_name)}', arn: '{str(arn)}, region: {str(region)}'}})
+        """
+    # query_enter_identity = f"""
+    #    MERGE({str(entity_source_name)}: Service{{name: {str(entity_source_name)},, arn: {str(arn)}, region: {str(region)} }})
+    #    ON CREATE
+    #    SET {str(entity_source_name)}.created = timestamp()
+    #    ON MATCH
+    #    SET
+    #    {str(entity_source_name)}.lastSeen = timestamp()
+    #    RETURN {str(entity_source_name)}.name, {str(entity_source_name)}.created, {str(entity_source_name)}.lastSeen
+    #    """
 
-    ans = data_base_connection.query(query_enter_identity, db='overwatchDataBase')
+    data_base_connection.query(query_enter_identity, db='overwatchDataBase')
+    print_node(data_base_connection, entity_source_name)
     return
 
 
 def link_parser(data_base_connection, entity_name, entity_source_name, event_type, timestamp, source_ip, user_agent):
     query_enter_identity = f""""
     (MATCH(Service:{str(entity_source_name)}) - 
-    [:{str(event_type)}{{timestamp:{str(timestamp)},source_ip:{str(source_ip)},user_agent:{str(user_agent)}}}] 
+    [:{str(event_type)}{{timestamp:{str(timestamp)},source_ip:{str(source_ip.replace(".","-"))},user_agent:{str(user_agent)}}}] 
     -> (MATCH(Service:{str(entity_name)}
     """
     ans = data_base_connection.query(query_enter_identity, db='overwatchDataBase')
@@ -133,12 +144,12 @@ def taxonomy_layer(data, data_base_connection):
     user_identity_parser(data_base_connection=data_base_connection,
                          entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
                          entity_type=data['userIdentity']['type'],
-                         arn=data['requestParameters']['sourceArn'].replace(":","--"),
+                         arn=data['requestParameters']['sourceArn'].replace(":", "--"),
                          region=data['awsRegion'])
-    # event_source_parser(data_base_connection=data_base_connection,
-    #                     entity_source_name=data['eventSource'],
-    #                     arn=data['resources'][0]['ARN'].replace(":","--"),
-    #                     region=data['awsRegion'])
+    event_source_parser(data_base_connection=data_base_connection,
+                        entity_source_name=data['eventSource'].partition('.')[0],
+                        arn=data['resources'][0]['ARN'].replace(":", "--"),
+                        region=data['awsRegion'])
     # link_parser(data_base_connection=data_base_connection,
     #             entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
     #             entity_source_name=data['eventSource'],
@@ -149,35 +160,46 @@ def taxonomy_layer(data, data_base_connection):
     return
 
 
+def print_nodes(data_base_connection):
+    print_all_nodes_query = f"""
+    MATCH (n) RETURN (n)
+    """
+    nodes = data_base_connection.query(print_all_nodes_query, db='overwatchDataBase')
+    print("nodes:\n")
+    print(nodes)
+    print("\n")
+    return
+
+
 # Defining main function
 def main():
     print("Connecting to neo4j DB...")
     data_base_connection = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="1234")
     print("Connected")
-    print("show DataBases")
+
     show_me_the_databases = f"""
     SHOW
     DATABASES
     """
     ans = data_base_connection.query(query=show_me_the_databases, db="neo4j")
+    print("show database Before: CREATE OR REPLACE \n" + str(ans))
 
-    print(ans)
+
     data_base_connection.query("CREATE OR REPLACE DATABASE overwatchDataBase")
     ans = data_base_connection.query(query=show_me_the_databases, db="overwatchDataBase")
-
-    print(ans)
+    print("show database After: CREATE OR REPLACE \n" + str(ans))
+    print("\n")
     print("Taxonomy for 1 log entry")
     # graph = Graph("bolt://localhost:7687", auth=("superman", "pizza"))
-
-
 
     with open("logEntry.json", "r") as read_file:
         data = json.load(read_file)
     test = data['requestParameters']['sourceArn'].partition(":")[2]
     test = f"""{test}"""
     test.replace(":", "--")
-    print(test)
+    # print(test)
     taxonomy_layer(data, data_base_connection)
+    print_nodes(data_base_connection)
     print("complete")
     return
 

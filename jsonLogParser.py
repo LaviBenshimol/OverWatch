777f639e3d773594c4b0a507a3177305ca7b97fc
LaviBenshimol <57056914+LaviBenshimol@users.noreply.github.com>
2021-05-11 18:14:52 +0300

    created the methods: print_all_files_names insert_single_log_entry create_CSV_with_logs

--- jsonLogParser.py
+++ jsonLogParser.py
@@ -1,5 +1,4 @@
 from neo4j import __version__ as neo4j_version, GraphDatabase
-from pandas import DataFrame
 from py2neo import Graph, Node, Relationship, NodeMatcher
 import pandas as pd
 import json
@@ -40,6 +39,16 @@ class Neo4jConnection:
             if session is not None:
                 session.close()
         return response
+
+
+import re
+
+word_regex_pattern = re.compile("[^A-Za-z0-9]+")
+
+
+def camel(chars):
+    words = word_regex_pattern.split(chars)
+    return "".join(w.lower() if i is 0 else w.title() for i, w in enumerate(words))
 
 
 """    
@@ -95,10 +104,10 @@ def print_node(data_base_connection, name):
 def user_identity_parser(data_base_connection, entity_name, entity_type, arn, region):
     if not node_exist(data_base_connection, entity_name):
         return
-    # entity_type = "aws service"
-    # entity_node = Node(entity_type,name=entity_name,{arn=str(arn),region=str(region)})
+    oldarn = arn
+    arn = camel(arn)
     query_enter_identity = f"""
-    CREATE (n:{str(entity_type)} {{name: '{str(entity_name)}', arn: '{str(arn)}, region: {str(region)}'}})
+    CREATE (n:{str(arn)} {{type: '{str(entity_type)}', arn: '{str(arn)}, region: {str(region)}'}})
     """
     data_base_connection.query(query_enter_identity, db='overwatchDataBase')
     print_node(data_base_connection, entity_name)
@@ -117,17 +126,21 @@ def user_identity_parser(data_base_connection, entity_name, entity_type, arn, re
 def event_source_parser(data_base_connection, entity_source_name, arn, region):
     if not node_exist(data_base_connection, entity_source_name):
         return
+    oldarn = arn
+    arn = camel(arn)
     entity_type = "AWSService"  # TODO: how to figure out its entity type ? what label to put? to update?
     query_enter_identity = f"""
-        CREATE (n:{str(entity_type)} {{name: '{str(entity_source_name)}', arn: '{str(arn)}, region: {str(region)}'}})
+        CREATE (n:{str(arn)} {{type: '{str(entity_source_name)}', arn: '{str(arn)}, region: {str(region)}'}})
         """
     data_base_connection.query(query_enter_identity, db='overwatchDataBase')
     print_node(data_base_connection, entity_source_name)
     return
 
 
-def link_parser(data_base_connection, entity_name, entity_source_name, event_type, timestamp, source_ip, user_agent):
+def link_parser(data_base_connection, arn_initiator, arn_actuator, entity_source_name, entity_name, event_type, timestamp, source_ip, user_agent):
     # TODO:retrive type ?
+    arn_initiator=camel(arn_initiator)
+    arn_actuator=camel(arn_actuator)
     entity_type = "AWSService"
     time_stamp = timestamp
     if event_type == "AwsApiCall":
@@ -136,8 +149,8 @@ def link_parser(data_base_connection, entity_name, entity_source_name, event_typ
         reltype = 'MANAGEMENT'
 
     query_enter_identity = f"""
-    MATCH (a:{entity_type}),(b:{entity_type})
-    WHERE a.name = '{entity_name}' AND b.name = '{entity_source_name}'
+    MATCH (a:{arn_initiator}),(b:{arn_actuator})
+    WHERE a.type = '{entity_name}' AND b.name = '{entity_source_name}'
     CREATE (a)-[relation:{reltype}]->(b)
     SET relation = {{Attributes:['{str(source_ip)}','{str(user_agent)}','{str(time_stamp)}']}}
     """
@@ -166,8 +179,10 @@ def taxonomy_layer(data, data_base_connection):
                         arn=data['resources'][0]['ARN'].replace(":", "--"),
                         region=data['awsRegion'])
     link_parser(data_base_connection=data_base_connection,
-                entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
+                arn_initiator=data['requestParameters']['sourceArn'].replace(":", "--"),
+                arn_actuator=data['resources'][0]['ARN'].replace(":", "--"),
                 entity_source_name=data['eventSource'].partition('.')[0],
+                entity_name=data['userIdentity']['invokedBy'].partition('.')[0],
                 event_type=data['eventType'],
                 timestamp=data['eventTime'],
                 source_ip=data['sourceIPAddress'],
@@ -242,7 +257,6 @@ def insert_single_log_entry(log_name, data_base_connection):
 
 
 def main():
-
     print("hello world")
     path_to_logs = "logFolder\\AWSLogs\\696714140038\\CloudTrail\\eu-central-1\\2021"
     # print_all_files_names(path_to_logs)
